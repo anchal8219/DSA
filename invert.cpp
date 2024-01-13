0a1c912c6ae3e82cea9d3a7269d9caee87d2bd37
Anchal <Anchal2011030@akgec.ac.in>
2024-01-13 20:23:55 +0530

    invert tree

--- invert.cpp
+++ invert.cpp
@@ -1,1 +1,15 @@
-kjaskak
+TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*>st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* n=st.top();
+            st.pop();
+            if(n!=NULL){
+                st.push(n->left);
+                st.push(n->right);
+                swap(n->left,n->right);
+            }
+        }
+        return root;
+    }
