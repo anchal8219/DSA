191267085e5ba22f7db46134d4feb59407e9529e
Anchal <anchal2727>
2024-02-15 01:08:00 +0530

    3 more different types of patterns added

--- pattern.cpp
+++ pattern.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 1.
 int main()
 {
 
@@ -11,13 +12,81 @@ int main()
 
     for (i = 1; i <= n; i++){
 
-        for (j = 1; j <= (n - i); j++)
+        for (j = 1; j <= i; j++)
+
+        
+        cout << "*";
+        
+
+    cout<< "\n";
+    }
+}
+
+
+// 2.
+int main()
+{
+
+    int i, n, j ;
+
+        cin >> n;
+
+    for (i = n; i >=1; i--){
+
+        for (j = 1; j <= i; j++)
+
+        
+        cout << "*";
+        
+
+    cout<< "\n";
+    }
+}
+
+
+// 3.
+int main()
+{
+
+    int i, n, j ;
+
+        cin >> n;
+
+    for (i = 1; i <= n; i++){
+
+        for (j = n-i; j >=1; j--)
 
         {
             cout << " ";
         }
 
-        for (int k = (n - i + 1); k <= n; k++)
+        for (int k = 1; k <= i; k++)
+
+        {
+            cout << "*";
+        }
+
+    cout<< "\n";
+    }
+}
+
+// 4.
+int main()
+{
+
+    int i, n, j ;
+
+        cin >> n;
+
+    for (i = n; i >= 1; i--){
+
+        for (j = n-i; j >=1; j--)
+
+        {
+            cout << " ";
+        }
+
+        for (int k = i; k >= 1; k--)
 
         {
             cout << "*";
