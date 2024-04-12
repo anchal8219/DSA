5bb86ea0af8e4c9f7f0fdc978df31bd094b8eaf7
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-12 21:07:18 +0530

    delete kth element from dll

--- daily_probs/doubly_ll.cpp
+++ daily_probs/doubly_ll.cpp
@@ -60,13 +60,45 @@ Node *deleteTail(Node *head){
        
        temp->next->prev=nullptr;
        temp->next=nullptr;
-       
-    //    temp->next->prev=temp;
        delete ws;
        return head;
        
 
     }
+}
+
+Node *deleteKth(Node *head,int k){
+    if(head==nullptr) return head;
+
+    int c=0;
+    Node *temp = head;
+    while(temp!=nullptr){
+        c++;
+        if(c==k) break;
+        temp=temp->next;
+    }
+    Node *back = temp->prev, *front=temp->next;
+
+
+    if(back==nullptr && front==nullptr){
+        delete(temp);
+        return nullptr;
+    }
+    else if(back==nullptr){
+        return deleteHead(head);
+    }
+    else if(front==nullptr){
+        return deleteTail(head);
+    }
+    else{
+        back->next = front;
+        front->prev = back;
+        temp->next=nullptr;
+        temp->prev = nullptr;
+        free(temp);
+        return head;
+    }
+
 }
 
 int main(){
@@ -76,14 +108,11 @@ int main(){
 
     // head = deleteHead(head);
     // print(head);
-    head = deleteTail(head);
+    // head = deleteTail(head);
+    // print(head);
+
+    head = deleteKth(head,5);
     print(head);
-    
-    
-    
-
-
-
     
     
 }
