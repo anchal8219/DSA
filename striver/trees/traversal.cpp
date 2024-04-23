048de2bb1486c16f23d9d668fde5e145e2ad5e55
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-23 11:42:11 +0530

    tree traversal using recursion

---
+++ striver/trees/traversal.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Tree{
+    public:
+    int data;
+    Tree *left;
+    Tree *right;
+
+    //contructor initialised
+    Tree(int x){
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inorder_traversal (Tree*root){
+    if (root == nullptr)
+    return;
+    inorder_traversal (root->left);
+    cout<<root->data<<" ";
+    inorder_traversal (root->right);
+
+}
+
+void preorder_traversal(Tree*root){
+    cout<<root->data<<" ";
+    preorder_traversal(root->left);
+    preorder_traversal(root->right);
+}
+
+void postorder_traversal(Tree*root){
+    preorder_traversal(root->left);
+    preorder_traversal(root->right);
+    cout<<root->data<<" ";
+}
+
+int main(){
+    Tree *root = new Tree(15);
+    root->left = new Tree (12);
+    root->right = new Tree (54);
+    root->left->left = new Tree (8);
+    root->left->right = new Tree (13);
+    root->left->left->left = new Tree (5);
+    root->right->left = new Tree (18);
+    inorder_traversal (root);
+    cout<<endl;
+    preorder_traversal(root);
+    cout<<endl;
+    postorder_traversal(root);
+    return 0;
+
+
+}
