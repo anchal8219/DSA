f7353e14d8b1625d8150ad130bbaef5575894d91
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-27 00:53:14 +0530

    snake pattern of matrix done

--- tcs_nqt/matrix.cpp
+++ tcs_nqt/matrix.cpp
@@ -1,35 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void transpose(vector<vector<int>> a)
-{
-    int m = a.size();
-    int n = a[0].size();
+// void transpose(vector<vector<int>> a)
+// {
+//     int m = a.size();
+//     int n = a[0].size();
 
-    // for (int i = 0; i < m; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         swap(a[i][j], a[j][i]);
-    //     }
-    // }
-    // for (int i = 0; i < m; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         cout << a[j][i] << " ";
-    //     }
-    //     cout << endl;
-    // }
+//     // for (int i = 0; i < m; i++)
+//     // {
+//     //     for (int j = 0; j < n; j++)
+//     //     {
+//     //         swap(a[i][j], a[j][i]);
+//     //     }
+//     // }
+//     // for (int i = 0; i < m; i++)
+//     // {
+//     //     for (int j = 0; j < n; j++)
+//     //     {
+//     //         cout << a[j][i] << " ";
+//     //     }
+//     //     cout << endl;
+//     // }
 
-    //or
-    for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-        cout << a[j][i] << " ";
+//     //or
+//     for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < m; j++) {
+//         cout << a[j][i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// }
+
+void printSnakepattern(vector<vector<int>>a){
+    int m=a.size();
+    int n=a[0].size();
+    for(int i=0;i<m;i++){
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                cout<<a[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+            for(int j=n-1;j>=0;j--){
+                cout<<a[i][j]<<" ";
+            }
+            cout<<endl;
+        }
     }
-    cout << endl;
-}
-
 }
 
 int main()
@@ -47,7 +66,10 @@ int main()
         }
     }
 
-    cout << "Transpose of the matrix:" << endl;
-    transpose(a);
+    // cout << "Transpose of the matrix:" << endl;
+    // transpose(a);
+
+    cout << "Snake Pattern of the matrix:" << endl;
+    printSnakepattern(a);
     return 0;
 }
