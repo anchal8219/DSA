c41aa95463f7e8b036aad057308363e4ac1ba297
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-27 01:05:41 +0530

    boundary of matrix done

--- tcs_nqt/matrix.cpp
+++ tcs_nqt/matrix.cpp
@@ -32,23 +32,41 @@ using namespace std;
 
 // }
 
-void printSnakepattern(vector<vector<int>>a){
-    int m=a.size();
-    int n=a[0].size();
-    for(int i=0;i<m;i++){
-        if(i%2==0){
-            for(int j=0;j<n;j++){
-                cout<<a[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        else{
-            for(int j=n-1;j>=0;j--){
-                cout<<a[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
+// void printSnakepattern(vector<vector<int>>a){
+//     int m=a.size();
+//     int n=a[0].size();
+//     for(int i=0;i<m;i++){
+//         if(i%2==0){
+//             for(int j=0;j<n;j++){
+//                 cout<<a[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         else{
+//             for(int j=n-1;j>=0;j--){
+//                 cout<<a[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//     }
+// }
+
+
+void printBoundary(vector<vector<int>>a){
+    int r=a.size();
+    int c=a[0].size();
+
+    for(int j=0;j<c;j++)
+    cout<<a[0][j]<<" ";
+
+    for(int i=1;i<r;i++)
+    cout<<a[i][c-1]<<" ";
+
+    for(int j=c-2;j>=0;j--)
+    cout<<a[r-1][j]<<" ";
+
+    for(int i=r-2;i>0;i--)
+    cout<<a[i][0]<<" ";
 }
 
 int main()
@@ -69,7 +87,11 @@ int main()
     // cout << "Transpose of the matrix:" << endl;
     // transpose(a);
 
-    cout << "Snake Pattern of the matrix:" << endl;
-    printSnakepattern(a);
+    // cout << "Snake Pattern of the matrix:" << endl;
+    // printSnakepattern(a);
+
+    cout << "Boundary of the matrix:" << endl;
+    printBoundary(a);
+
     return 0;
 }
