eef1e04308ce4ca612cc47f82941c2be997ae09e
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-26 10:58:27 +0530

    sieve of erathos

--- infosys/prime(sieve).cpp
+++ infosys/prime(sieve).cpp
@@ -36,16 +36,36 @@ bool isPrime(int n){
     return true;
 }
 
+// 4. sieve approach
+void sieve(int n){
+    vector<bool>isPrime(n+1,true);
+
+    for(int i=2;i*i<=n;i++){
+        if(isPrime[i]){
+            for(int j=2*i;j<=n;j=j+i)
+            isPrime[j] = false;
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(isPrime[i])
+        cout<<i<<" ";
+    }
+}
+//TC: O(n log log n)
+
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-    cin>>n;
-    bool ans = isPrime(n);
-    if(ans) cout<<"prime number"<<endl;
-    else cout<<"not a prime number"<<endl;
-    }
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     int n;
+    // cin>>n;
+    // bool ans = isPrime(n);
+    // if(ans) cout<<"prime number"<<endl;
+    // else cout<<"not a prime number"<<endl;
+    // }
+    int m;
+    cin>>m;
+    sieve(m);
     
 }
