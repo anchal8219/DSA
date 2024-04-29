a7bcea2757264cc90ea0cbb78cf9d5f1e8123254
anchal8219 <anchal2011030@akgec.ac.in>
2024-04-29 19:38:25 +0530

    count set bits using Briar Kerningams algo

--- tcs_nqt/2nd_shift.cpp
+++ tcs_nqt/2nd_shift.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int countSetBits(int n){
+    // int c=0;
+    // while(n>0){
+    //     if((n&1)==1)
+    //     c++;
+    //     n=n>>1;
+    // }
+    // return c;
+
+    //or
     int c=0;
     while(n>0){
-        if((n&1)==1)
+        n = n&(n-1);
         c++;
-        n=n>>1;
-    }
+    } 
     return c;
 }
 
