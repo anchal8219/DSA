99d6db89ee83016927e09bb407c234932801e151
anchal8219 <anchal2011030@akgec.ac.in>
2024-05-15 01:23:04 +0530

    max depth of a BT(brute force approach)

--- striver/trees/max_depth.cpp
+++ striver/trees/max_depth.cpp
@@ -28,39 +28,28 @@ public:
 
 
 //brute force approach
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if (root == nullptr) return 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        int c=0;
-        while(!q.empty()){
-            int sz = q.size();
-            c++;
-            for(int i=0;i<sz;i++){
-                TreeNode* top = q.front();
-                q.pop();
-                if(top->left!=nullptr) q.push(top->left);
-                if(top->right!=nullptr) q.push(top->right);
-            } 
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if (root == nullptr) return 0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         int c=0;
+//         while(!q.empty()){
+//             int sz = q.size();
+//             c++;
+//             for(int i=0;i<sz;i++){
+//                 TreeNode* top = q.front();
+//                 q.pop();
+//                 if(top->left!=nullptr) q.push(top->left);
+//                 if(top->right!=nullptr) q.push(top->right);
+//             } 
 
-        }
-        return c;
+//         }
+//         return c;
         
-    }
-};
+//     }
+// };
 
 
 int main() {
